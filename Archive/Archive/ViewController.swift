ea9fe197baeca52d9d8aad037ffc919ece44ea1c
阳君 <937447974@qq.com>
2019-07-11 14:44:35 +0800

    feat: 打包机完成

---
+++ Archive/Archive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Archive
+//
+//  Created by 阳君 on 2019/7/11.
+//  Copyright © 2019 YJCocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+#source "https://gems.ruby-china.com"
+
+#gem 'carthage'
+gem 'cocoapods'
+gem 'fastlane'
+
+plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
+eval_gemfile(plugins_path) if File.exist?(plugins_path)
